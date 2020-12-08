be20a9edf4875e6bbcc57785f4db6dd8d36b120f
Scott Black <scott.black@usu.edu>
2020-12-08 09:59:36 -0700

    add other packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='hs_rdf',
     version='0.1',
-    packages=['hs_rdf'],
+    packages=['hs_rdf', 'hs_rdf.implementations', 'hs_rdf.implementations.hydroshare', hs_rdf.schemas', 'hs_rdf.namespaces'],
     url='https://github.com/sblack-usu/hs_rdf',
     license='',
     author='Scott Black',
