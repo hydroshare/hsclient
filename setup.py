16247165265ecd866eee1859e930657e109b8307
Scott Black <scott.black@usu.edu>
2020-12-08 11:23:22 -0700

    add install requirements to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name='hs_rdf',
     version='0.1',
     packages=find_packages(include=['hs_rdf', 'hs_rdf.*']),
+    install_requires=[
+        'rdflib == 5.0.0',
+        'requests == 2.24.0',
+        'pydantic == 1.7.2'
+    ],
     url='https://github.com/sblack-usu/hs_rdf',
     license='',
     author='Scott Black',
