eda2ab5b2af12c1c019abd970fde8405e855915c
Scott Black <scott.black@usu.edu>
2020-12-08 08:44:41 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='hs_rdf',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/sblack-usu/hs_rdf',
+    license='',
+    author='Scott Black',
+    author_email='scott.black@usu.edu',
+    description='A python client for managing hydroshare resources'
+)
