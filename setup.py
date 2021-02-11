6f213961ea01684ba3b03e8e12491ed502c7b84c
Scott Black <scott.black@usu.edu>
2021-02-10 17:56:10 -0700

    add license

--- setup.py
+++ setup.py
@@ -3,15 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name='hsclient',
     version='0.1',
-    packages=find_packages(include=['hsclient', 'hsclient.*', 'hsclient.schemas.*', 'hsclient.schemas.rdf.*']),
+    packages=find_packages(include=['hsclient', 'hsclient.*', 'hsclient.schemas.*', 'hsclient.schemas.rdf.*'],
+                           exclude=("tests",)),
     install_requires=[
         'hsmodels',
         'requests',
         'pandas'
     ],
-    url='https://github.com/sblack-usu/hsclient',
+    url='https://github.com/hydroshare/hsclient',
     license='',
     author='Scott Black',
     author_email='scott.black@usu.edu',
-    description='A python client for managing hydroshare resources'
+    description='A python client for managing HydroShare resources'
 )
