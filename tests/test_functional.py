9db9855ca9976ce19c3950195e6b5c392891e660
Scott Black <scott.black@usu.edu>
2021-02-19 13:17:44 -0700

    test against beta

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -17,7 +17,7 @@ def change_test_dir(request):
 
 @pytest.fixture()
 def hydroshare(change_test_dir):
-    hs = HydroShare(os.getenv("HYDRO_USERNAME"), os.getenv("HYDRO_PASSWORD"))
+    hs = HydroShare(os.getenv("HYDRO_USERNAME"), os.getenv("HYDRO_PASSWORD"), host="beta.hydroshare.org")
     return hs
 
 
