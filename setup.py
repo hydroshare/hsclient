567e8e98ffb37e75063e1e61a0260a5d0aa77394
Scott Black <scott.black@usu.edu>
2021-04-15 09:26:15 -0600

    pick up latest hsmodels

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ README = (pathlib.Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='hsclient',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(include=['hsclient', 'hsclient.*'],
                            exclude=("tests",)),
     install_requires=[
-        'hsmodels==0.1.2',
+        'hsmodels==0.1.3',
         'requests',
         'requests_oauthlib',
         'pandas'
