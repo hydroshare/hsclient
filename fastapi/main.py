55a7546bf460fc13bc78b59b0a5eb3801569914f
Scott Black <scott.black@usu.edu>
2021-04-16 09:37:40 -0600

    load hydroshare form from fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -3,20 +3,28 @@ from fastapi import FastAPI
 
 from hsclient.hydroshare import HydroShare
 from hsmodels.schemas.resource import ResourceMetadata
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-hs = HydroShare('admin', 'default')
+app.mount("/czhub", StaticFiles(directory="czhub"), name="czhub")
+
+hs = HydroShare('sblack', 'j1u2n3o4')
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/resource/{resource_id}", response_model=ResourceMetadata, response_model_by_alias=False, response_model_exclude_none=True)
+@app.get("/resource/{resource_id}/", response_model=ResourceMetadata)
 def resource_metadata(resource_id: str):
     res = hs.resource(resource_id)
-    return res.metadata
+    metadata = res.metadata
+    return metadata
 
+
+@app.get("/schema/{schema_type}/")
+def schema_hydroshare(schema_type: str):
+    return ResourceMetadata.schema()
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)
