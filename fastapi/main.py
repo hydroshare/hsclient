ebab3bc4cf2372af28057ee94a5e83d295819983
Scott Black <scott.black@usu.edu>
2021-04-19 17:34:35 -0600

    setup orcid login and hydroshare authorization

--- fastapi/main.py
+++ fastapi/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Depends, Request, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from starlette.config import Config
 from starlette.middleware.sessions import SessionMiddleware
-from starlette.responses import HTMLResponse, RedirectResponse
+from starlette.responses import HTMLResponse, RedirectResponse, JSONResponse
 
 from hsclient.hydroshare import HydroShare
 from hsmodels.schemas.resource import ResourceMetadata
@@ -23,38 +23,79 @@ oauth.register(name='hydroshare',
                authorize_url="https://www.hydroshare.org/o/authorize/",
                token_endpoint="https://www.hydroshare.org/o/token/")
 oauth.register(name='orcid',
-               authorize_url='',
-               token_endpoint='')
+               authorize_url='https://sandbox.orcid.org/oauth/authorize',
+               token_endpoint='https://sandbox.orcid.org/oauth/token',
+               client_kwargs={'scope': 'openid'})
+
+db = {}
 
 def _hs(token: str):
     return HydroShare(token=token, client_id=config.get("HYDROSHARE_CLIENT_ID"))
 
 def hs(request: Request):
-    access_token = request.session.get("access_token")
+    orcid = request.session.get("orcid")
+    if not orcid:
+        raise HTTPException(status_code=401, detail="Unauthorized, not logged into orcid")
+    access_token = db[orcid]["hs_access_token"]
     if not access_token:
-        raise HTTPException(status_code=401, detail="Unauthorized")
+        raise HTTPException(status_code=401, detail="Unauthorized, HydroShare account not registered")
     return _hs(access_token)
+
+@app.route('/')
+def home(request: Request):
+    orcid = request.session.get('orcid')
+    if orcid:
+        if orcid in db:
+            return JSONResponse(content={orcid: db[orcid]})
+        return JSONResponse(content={"status": f"{orcid} not recognized"})
+    return JSONResponse(content={"status": "Not logged in"})
 
 @app.route('/login')
 async def login(request: Request):
     redirect_uri = request.url_for('auth')
+    return await oauth.orcid.authorize_redirect(request, redirect_uri)
+
+@app.route('/authorize/hydroshare')
+async def login_hydroshare(request: Request):
+    orcid = request.session.get('orcid')
+    if not orcid:
+        return RedirectResponse("/login")
+    redirect_uri = request.url_for('auth_hydroshare')
     return await oauth.hydroshare.authorize_redirect(request, redirect_uri)
 
 
 @app.route('/auth')
 async def auth(request: Request):
     try:
+        token = await oauth.orcid.authorize_access_token(request)
+    except OAuthError as error:
+        return HTMLResponse(f'<h1>{error.error}</h1>')
+    if token['orcid'] in db:
+        db[token['orcid']]['name'] = token['name']
+        db[token['orcid']]['access_token'] = token['access_token']
+    else:
+        db[token['orcid']] = {"name": token['name'], 'access_token': token['access_token']}
+    request.session['orcid'] = token['orcid']
+    return RedirectResponse(url='/')
+
+@app.route('/auth/hydroshare')
+async def auth_hydroshare(request: Request):
+    try:
         token = await oauth.hydroshare.authorize_access_token(request)
     except OAuthError as error:
         return HTMLResponse(f'<h1>{error.error}</h1>')
-    request.session['access_token'] = token['access_token']
-    return RedirectResponse(url='/czhub/portal.html')
+    orcid = request.session.get('orcid')
+    if not orcid:
+        raise HTTPException(status_code=400, detail="No logged in with an orcid, cannot authorize hydroshare")
+    db[orcid]["hs_access_token"] = token['access_token']
+    return RedirectResponse(url='/')
+
 
 
 @app.route('/logout')
 async def logout(request: Request):
-    request.session.pop('access_token', None)
-    return RedirectResponse(url='/czhub/portal.html')
+    request.session.pop('orcid', None)
+    return RedirectResponse(url='/')
 
 @app.get("/resource/{resource_id}/", response_model=ResourceMetadata)
 def resource(request: Request, resource_id: str):
