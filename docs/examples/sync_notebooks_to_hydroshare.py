09ead740f161238b1544bafaf50d6d4ae6201812
pkdash <pkdash_reena@hotmail.com>
2024-04-17 12:37:23 -0400

    [#62] fixing script to sync notebooks to hydroshare

--- docs/examples/sync_notebooks_to_hydroshare.py
+++ docs/examples/sync_notebooks_to_hydroshare.py
@@ -8,7 +8,7 @@ res = hs.resource("7561aa12fd824ebb8edbee05af19b910")
 for root, directories, files in os.walk("."):
     root = root[2:]
     file_paths = [os.path.join(root, f) for f in files if f[0] != "." and f.endswith(".ipynb")]
-    if not res.file(folder=root):
+    if root and not res.file(folder=root):
         res.folder_create(root)
     res.file_upload(*file_paths, destination_path=root)
     break
