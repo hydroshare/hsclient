39ba0cba7cf7986286cdf59a8e50e8d67f48164b
Pabitra Dash <pabitra.dash@usu.edu>
2025-02-24 16:01:46 -0500

    bumping version to 1.1.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ dev_deps = ["pytest", "pytest-xdist", "pytest-cov", "mkdocs", "mknotebooks", "mk
 
 setup(
     name='hsclient',
-    version='1.1.4',
+    version='1.1.5',
     packages=find_packages(include=['hsclient', 'hsclient.*'],
                            exclude=("tests",)),
     install_requires=[
