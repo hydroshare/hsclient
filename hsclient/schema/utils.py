d49b00b384173f86c17bf1e56e371a0da3f1951e
pkdash <pabitra.dash@usu.edu>
2026-04-19 16:58:44 -0400

    Using the new metadata adapter in the helper load_json function

--- hsclient/schema/utils.py
+++ hsclient/schema/utils.py
@@ -1,22 +1,22 @@
 from typing import Any, Dict, Union
-from hsmodels.schemas.resource import ResourceMetadata
 
-from .adapter import from_core_metadata
-from .core import CoreMetadata
+from ..metadata_adapter.legacy_resource_models import LegacyResourceMetadata
+from ..metadata_adapter.adapter import MetadataAdapter
 from .base import Place, GeoShape
 from .dataset import ScientificDataset
 from pydantic import ValidationError
 
-def load_json(json_data: Dict[str, Any], data_path: str) -> Union[ResourceMetadata, ScientificDataset]:
-    """Load JSON metadata into the appropriate schema model based on file path."""
+def load_json(json_data: Dict[str, Any], data_path: str) -> Union[LegacyResourceMetadata, ScientificDataset]:
+    """Loads JSON metadata into the appropriate schema model based on file path."""
     # TODO: PK: hydroshare needs to be fixed so that relation has the valid data.
-    if 'relation' in json_data:
-        json_data['relation'] = []
+    # if 'relation' in json_data:
+    #     json_data['relation'] = []
 
     if data_path.endswith("dataset_metadata.json"):
-        return from_core_metadata(CoreMetadata.model_validate(json_data))
+        return MetadataAdapter.to_legacy_resource_metadata(json_data)
 
     try:
+        # Content type/aggregation metadata
         return ScientificDataset.model_validate(json_data)
     except ValidationError:
         spatial_coverage = json_data.get('spatialCoverage')
@@ -25,10 +25,11 @@ def load_json(json_data: Dict[str, Any], data_path: str) -> Union[ResourceMetada
             geo = spatial_coverage['geo']
             if geo and geo['type'] == 'GeoShape':
                 geo_model = GeoShape(box=geo['box'], validate_bbox=False)
-                spatial_coverage_model = Place(name=spatial_coverage.get('name'), srs=spatial_coverage.get('srs'), geo=geo_model)
-                # spatial_coverage_model.geo = geo_model
+                place = Place(name=spatial_coverage.get('name'), srs=spatial_coverage.get('srs'), geo=geo_model)
+                spatial_coverage_model = place
                 json_data['spatialCoverage'] = None
         content_type_metadata= ScientificDataset.model_validate(json_data)
         content_type_metadata.spatialCoverage = spatial_coverage_model
-        # TODO: PK: Need to convert an instance of ScientificDataset to corresponding content type metadata model in hsmodels.schemas.aggregations
+        # TODO: PK: Need to convert an instance of ScientificDataset to corresponding
+        # content type legacy metadata model
         return content_type_metadata
